97e0c1db35fe4880cc881d91c7b3231da01d3a95
jean carlos hernandez <jeancarloshernadez@gmail.com>
2024-10-25 20:28:26 -0500

    Task schema, create task

--- api.js
+++ api.js
@@ -1,6 +1,9 @@
 var mongoose = require("mongoose");
 var express = require("express");
 var router = express.Router();
+var TaskModel = require('./task_schema');
+
+mongoose.set('strictQuery', false); 
 
 let environment = null;
 
@@ -34,4 +37,27 @@ mongoose.connect(db, {
     }
 });
 
+
+router.post('/create-task', function (req, res) {
+    let task_id = req.body.TaskId;
+    let name = req.body.Name;
+    let deadline = req.body.Deadline;
+
+    let task = {
+        TaskId: task_id,
+        Name: name,
+        Deadline: deadline
+    }
+    var newTask = new TaskModel(task);
+
+    newTask.save(function (err, data) {
+        if (err) {
+            console.log(err);
+            res.status(500).send("Internal error\n");
+        }
+        else {
+            res.status(200).send("OK\n");
+        }
+    });
+});
 module.exports = router;
